f3964e69a13e1cb0ffa56dc43ee927588e6d0b15
Jeremia-Odoo-etech <j.rafanomezantsoa@etechconsulting-mg.com>
2025-03-09 01:35:05 +0300

    [FIX] fix error not found

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require("express")
 var app = express()
+const cors = require('cors');
+require('dotenv').config();
+
+app.use(cors());
 
 app.get("/", function (req, res){
     res.setHeader("Content-type", "text/plain")
@@ -11,6 +15,8 @@ app.get("/upload", function (req, res){
     res.send("Uploaded successfuly")
 })
 
-app.listen(process.env.PORT || 3000, ()=> {
-    console.log("http://localhost:3000")
+app.listen(process.env.PORT || 8080, ()=> {
+    // console.log("http://localhost:3000")
 })
+
+module.exports = app;
