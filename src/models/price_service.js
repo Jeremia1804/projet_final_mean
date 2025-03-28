2557cc47e1525e7f3580e8f7c7c945fd86cef888
Jeremia-Odoo-etech <j.rafanomezantsoa@etechconsulting-mg.com>
2025-03-29 02:06:36 +0300

    [ADD] add model for price service

---
+++ src/models/price_service.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const PriceSchema = new mongoose.Schema(
+  {
+    service: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Service",
+      required: true,
+    },
+    model: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Model",
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: [0, "Price must be greater than 0"]
+    },
+  },
+  { timestamps: true }
+);
+
+class PriceServiceModel extends mongoose.model("PriceService", PriceSchema) {
+  static async setPrice(service, model, price) {
+    const existingPrice = await PriceServiceModel.findOne({ service, model });
+
+    if (existingPrice) {
+      existingPrice.price = price;
+      return await existingPrice.save();
+    } else {
+      const newPrice = new PriceServiceModel({ service, model, price });
+      return await newPrice.save();
+    }
+  }
+
+  static async getAllPrices() {
+    return await PriceServiceModel.find().populate("service").populate("model");
+  }
+
+  static async deletePrice(id) {
+    return await PriceServiceModel.findByIdAndDelete(id);
+  }
+}
+
+module.exports = PriceServiceModel ;
